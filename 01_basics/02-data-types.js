a105ee598d01e93332565d6e1706261745fe32c9
vivekmaniyar <vivekmaniyar9601@gmail.com>
2024-03-19 23:31:30 +0530

    dataTypes learning done

---
+++ 01_basics/02-data-types.js
@@ -0,0 +1,11 @@
+"use strict"; // Treat All JS Code As Newer Version
+
+let name = 'Vivek';
+let age = 22;
+let isLoggedIn;
+
+console.log(typeof name); // string
+console.log(typeof age); // number
+console.log(typeof isLoggedIn); // undefined
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

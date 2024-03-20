2ff91850b9e8fbd063befdc83c0a74c38ba7f30f
vivekmaniyar <vivekmaniyar9601@gmail.com>
2024-03-20 23:20:29 +0530

    conversion operation learning done

---
+++ 01_basics/03-conversion-operation.js
@@ -0,0 +1,27 @@
+let score = false;
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = -565;
+
+// any numeric value other than 0 => true; 0 => false;
+// "" => false
+// "Vivek" => true
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let someNumber = -24;
+
+// 24 => "24"
+let stringNumber = String(someNumber);
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
